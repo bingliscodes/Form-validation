3abfa7bc243e1159c543386f3f3b55905cec1dea
Benjamin Inglis <bjammin134@gmail.com>
2025-06-06 11:21:26 -0700

    Validate email with regular expression

--- src/util/validation.js
+++ src/util/validation.js
@@ -15,5 +15,9 @@ export function validateDOB(date) {
 }
 
 export function validateEmail(email) {
-  return email.includes('@');
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
